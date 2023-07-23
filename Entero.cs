d2c6afda320ee2a892a7c274c988e66d5c830bd0
Matteo Espinoza <arthasxl123456@gmail.com>
2023-07-23 12:08:19 -0400

    invertir

--- Entero.cs
+++ Entero.cs
@@ -53,5 +53,17 @@ namespace VectorRepaso
 
             return true;
         }
+        public void Invertir()
+        {
+            int dig,
+                num = 0;
+            while (n > 0)
+            {
+                dig = n % 10;
+                n = n / 10;
+                num = num * 10 + dig;
+            }
+            this.n = num;
+        }
     }
 }
