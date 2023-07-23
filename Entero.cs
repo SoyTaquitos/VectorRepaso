0c0f38db3535d8f8c859e0870a61cc4f63763ec1
Matteo Espinoza <arthasxl123456@gmail.com>
2023-07-23 12:06:17 -0400

    Clases

---
+++ Entero.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorRepaso
+{
+    class Entero
+    {
+        private int n;
+        public Entero()
+        {
+            n = 0;
+        }
+        public Entero(int x)
+        {
+            n = x;
+        }
+        public void Cargar(int x)
+        {
+            n = x;
+        }
+        public bool VerificarFibo()
+        {
+            int a = -1;
+            int b = 1;
+            int c = a + b;
+            while (c < n)
+            {
+                a = b;
+                b = c;
+                c = a + b;
+            }
+            if (c == n)
+                return true;
+            return false;
+        }
+    }
+}
