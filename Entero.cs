00d3c6aecc59f57fa4e7dbf28474a0fd113017f4
Matteo Espinoza <arthasxl123456@gmail.com>
2023-07-23 12:06:57 -0400

    clases

--- Entero.cs
+++ Entero.cs
@@ -36,5 +36,22 @@ namespace VectorRepaso
                 return true;
             return false;
         }
+        public bool EsPrimo()
+        {
+            if (n < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
